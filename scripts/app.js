edc32ce880a66b13c3c065cb434d9c5f29f8d294
Joseph Kim <joseph.kim.kr@gmail.com>
2017-08-18 08:24:50 -0700

    adds image carousel to main page

--- scripts/app.js
+++ scripts/app.js
@@ -14,11 +14,38 @@ portfolio.handle = function() {
 portfolio.handleClick = function() {
   $('i').on('click', function() {
     $('#tabs li').toggle();
-    // $('#tabs li').css('display', 'inline');
+    $('#tabs li').css('display', 'inline');
   });
+};
+
+portfolio.carousel = function() {
+  var index = 0;
+  var imgList = [$('#1'), $('#2'), $('#3')];
+
+  $('#right-button').on('click', function() {
+    index++;
+    $('.slide').hide();
+    if (index === 3) {
+      index = 0;
+    }
+    imgList[index].show();
+  });
+
+  $('#left-button').on('click', function() {
+    index--;
+    $('.slide').hide();
+    if (index === -1) {
+      index = 2;
+    }
+    imgList[index].show();
+  });
+
+  $('.slide').hide();
+  imgList[index].show();
 };
 
 $(document).ready(function() {
   portfolio.handle();
   portfolio.handleClick();
+  portfolio.carousel();
 });
